30a5934f26936d798ad1158aacc379bc3e517fe4
duski96 <duski96@naver.com>
2025-02-17 13:44:29 +0900

    배경 이미지, 클래스 추가 관련 코드 수정

--- project02/src/components/sub/SubMain.jsx
+++ project02/src/components/sub/SubMain.jsx
@@ -1,10 +1,9 @@
 import './SubMain.css';
-import subMainBg from './../../assets/sub/interest_main_img01.jpg';
 
-const SubMain=({title, explanation})=>{
+const SubMain=({title, explanation, background, addClassName})=>{
     return (
-        <section className='Main sub'>
-            <img src={subMainBg} alt='background' />
+        <section className={`Main sub ${addClassName}`}>
+            <img src={background} alt='background' />
             <div className='inner_1280'>
                 <h2 className='fs_lg mb_sm NotoSansKR'>{title}</h2>
                 <h3 className='fs_md NotoSansKR'>{explanation}</h3>

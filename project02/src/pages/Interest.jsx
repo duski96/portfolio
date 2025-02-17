ff1349dd582b12cbbc70707ca2f1aacaf79416ea
duski96 <duski96@naver.com>
2025-02-17 13:49:06 +0900

    배경 이미지, 클래스 지정 관련 코드 수정

--- project02/src/pages/Interest.jsx
+++ project02/src/pages/Interest.jsx
@@ -4,11 +4,13 @@ import InterestList from '../components/sub/InterestList';
 import QuickMenu from '../components/QuickMenu';
 import Footer from '../components/Footer';
 
+import subMainBg from './../assets/sub/interest_main_img01.jpg';
+
 const Interest=()=>{    
     return (
         <>
             <Header isActive={true}/>
-            <SubMain title={'관심 매물'} explanation={'관심 목록에 추가된 매물입니다.'}/>
+            <SubMain title={'관심 매물'} explanation={'관심 목록에 추가된 매물입니다.'} background={subMainBg} addClassName={'Interest'} />
             <InterestList />
             <QuickMenu />
             <Footer />

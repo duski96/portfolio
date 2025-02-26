f913c76d0eec74f59f2401d6a55da37534252ec3
duski96 <duski96@naver.com>
2025-02-26 21:14:10 +0900

    .env 파일 경로 지정

--- project02/server/server.js
+++ project02/server/server.js
@@ -6,7 +6,12 @@ const app=express();
 import api from './routes/index.js';
 app.use('/api', api);
 
-const PORT=import.meta.env.VITE_PORT;
+// node.js 환경에서는 import.meta.env 사용 X
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({path:path.resolve(process.cwd(), '../.env')}); //env 파일 경로 지정
+
+const PORT=process.env.VITE_PORT || 4000;
 
 app.listen(PORT, ()=>{
     console.log(`Server run : http://localhost:${PORT}/`);

c99c69fbd4095fe8bfcd27a68b5b1af4bd3a2351
duski96 <duski96@naver.com>
2025-03-12 03:45:30 +0900

    LifestylePost 컴포넌트에 tableName 속성 추가

--- project02/src/components/sub/brand/Lifestyle.jsx
+++ project02/src/components/sub/brand/Lifestyle.jsx
@@ -22,28 +22,28 @@ const Lifestyle=()=>{
                             <span>정기 모임</span>
                             <button type="button" className='fs_xsm'><Link to={'/brand/lifestyle/meeting'}>+ 더보기</Link></button>
                         </h4>
-                        <LifestylePost visible={'part'} />
+                        <LifestylePost visible={'part'} tableName={'meeting'} />
                     </div>
                     <div className='board post'>
                         <h4 className='board_title fs_lg mb_md'>
                             <span>드라이빙 코스 추천</span>
                             <button type="button" className='fs_xsm'><Link to={'/brand/lifestyle/driving'}>+ 더보기</Link></button>
                         </h4>
-                        <LifestylePost visible={'part'} />
+                        <LifestylePost visible={'part'} tableName={'driving'} />
                     </div>
                     <div className='board post'>
                         <h4 className='board_title fs_lg mb_md'>
                             <span>정비 후기</span>
-                            <button type="button" className='fs_xsm'><Link to={'/brand/lifestyle/maintanance'}>+ 더보기</Link></button>
+                            <button type="button" className='fs_xsm'><Link to={'/brand/lifestyle/maintenance'}>+ 더보기</Link></button>
                         </h4>
-                        <LifestylePost visible={'part'} />
+                        <LifestylePost visible={'part'} tableName={'maintenance'} />
                     </div>
                     <div className='board post'>
                         <h4 className='board_title fs_lg mb_md'>
                             <span>결함/리콜</span>
                             <button type="button" className='fs_xsm'><Link to={'/brand/lifestyle/defect'}>+ 더보기</Link></button>
                         </h4>
-                        <LifestylePost visible={'part'} />
+                        <LifestylePost visible={'part'} tableName={'defect'} />
                     </div>
                 </div>
             </div>

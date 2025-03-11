eac11b2e3f0655f6e5cf959f0cb7aa0fb1dce4b7
duski96 <duski96@naver.com>
2025-03-12 03:46:44 +0900

    mock 데이터 주석 및 DB 데이터 연동

--- project02/src/components/sub/brand/LifestylePost.jsx
+++ project02/src/components/sub/brand/LifestylePost.jsx
@@ -1,51 +1,83 @@
-import profileImg from '../../../assets/sub/profile_default.jpg';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
-const mockPost=[
-    {
-        id:0,
-        userId:'user1',
-        userNickname:'말랑박쥐',
-        userCar:'클럽맨',
-        title:'제 1회 부산 폭주 모임',
-        content:'폭주합시다.'
-    },
-    {
-        id:1,
-        userId:'user2',
-        userNickname:'딱딱구리',
-        userCar:'5도어',
-        title:'제 1회 부산 폭주 모임',
-        content:'폭주합시다.'
-    },
-    {
-        id:2,
-        userId:'user3',
-        userNickname:'콩송편은적폐',
-        userCar:'컨트리맨',
-        title:'제 1회 부산 폭주 모임',
-        content:'폭주합시다.'
-    },
-    {
-        id:3,
-        userId:'user4',
-        userNickname:'undeFined',
-        userCar:'기타',
-        title:'제 1회 부산 폭주 모임',
-        content:'폭주합시다.'
-    }
-];
+let postData;
+axios.get('/api/board/post').then((res)=>{
+    postData=res.data;
+    console.log(postData['meeting'].length);
+}).catch((err)=>{
+    console.log(err);
+});
 
-const LifestylePost=({visible})=>{
+// const mockPost=[
+//     {
+//         id:0,
+//         userId:'user1',
+//         userNickname:'말랑박쥐',
+//         userCar:'클럽맨',
+//         title:'제 1회 부산 폭주 모임',
+//         content:'폭주합시다.'
+//     },
+//     {
+//         id:1,
+//         userId:'user2',
+//         userNickname:'딱딱구리',
+//         userCar:'5도어',
+//         title:'제 1회 부산 폭주 모임',
+//         content:'폭주합시다.'
+//     },
+//     {
+//         id:2,
+//         userId:'user3',
+//         userNickname:'콩송편은적폐',
+//         userCar:'컨트리맨',
+//         title:'제 1회 부산 폭주 모임',
+//         content:'폭주합시다.'
+//     },
+//     {
+//         id:3,
+//         userId:'user4',
+//         userNickname:'undeFined',
+//         userCar:'기타',
+//         title:'제 1회 부산 폭주 모임',
+//         content:'폭주합시다.'
+//     }
+// ];
+
+// postData는 review와 달리 네 개의 테이블을 한 번에 불러옴
+// tableName 파라미터에 따라 몇 번째 테이블을 불러올지 정해짐
+const LifestylePost=({visible, tableName})=>{
+    
+    // lifestyle 페이지가 아닌 해당 게시판 페이지라면 useParams로 불러올 데이터 구분
+    const boardName=useParams().boardName;
+
+    // lifestyle 페이지에선 tableName, 게시판 페이지에선 boardName이 속성으로 사용됨
+    let attr;
+    tableName ? attr=tableName : attr=boardName;
+
+    //post 페이지와 lifestyle 페이지에서 보여질 개수가 다르기 때문에 slice할 인덱스를 지정할 변수
     let slIdx;
-    visible==='part' ? slIdx=mockPost.length-3 : slIdx=0;
+
+    // 작성글 데이터의 개수에 따라 리턴이 다름
+    if(!postData[attr].length){
+        return <p>작성된 글이 없습니다.</p>
+    }
+    else if(postData[attr].length<=3){
+        // 작성글이 3개 이하면 모두 출력
+        slIdx=0;
+    }
+    else{
+        // 작성글이 2개 넘으면 lifestyle 페이지에선 3개만, 리뷰 페이지에선 모두 출력 
+        visible==='part' ? slIdx=postData[attr].length-3 : slIdx=0;
+    }
 
     return (
         <ul className='post_list fs_sm'>
-            {mockPost.slice(slIdx).reverse().map((item)=>
+            {postData[attr].slice(slIdx).reverse().map((item)=>
                 <li key={item.id} className='post_item'>
                     <ul>
                         <li className='mb_sm'><span className='title fs_md'>{item.title}</span></li>
-                        <li><span className='ninckname'>{item.userId}</span> <span className='car'>{item.userCar}</span></li>
+                        <li><span className='ninckname'>{item.nickname}</span> <span className='car'>{item.car}</span></li>
                     </ul>
                 </li>
             )}
--- project02/src/components/sub/brand/LifestyleReview.jsx
+++ project02/src/components/sub/brand/LifestyleReview.jsx
@@ -1,52 +1,72 @@
 import profileImg from '../../../assets/sub/profile_default.jpg';
+import axios from 'axios';
 
-const mockReview=[
-    {
-        id:0,
-        userId:'user1',
-        userNickname:'말랑박쥐',
-        userCar:'클럽맨',
-        rate:3,
-        content:'이 차를 사느니 걸어다니겠습니다.'
-    },
-    {
-        id:1,
-        userId:'user2',
-        userNickname:'딱딱구리',
-        userCar:'5도어',
-        rate:5,
-        content:'이정도면 개 혜자임.'
-    },
-    {
-        id:2,
-        userId:'user3',
-        userNickname:'콩송편은적폐',
-        userCar:'컨트리맨',
-        rate:5,
-        content:'미니는 역시 돈 보고 타는 차는 아니네요. 이게 배고픈게 없으면 나가 스트라이프 파티 참여하면 얼마를 타고 좋아요.'
-    },
-    {
-        id:3,
-        userId:'user4',
-        userNickname:'undeFined',
-        userCar:'기타',
-        rate:5,
-        content:'역시 차는 롤스로이스죠~'
-    }
-];
+let reviewData;
+
+axios.get('/api/board/review').then((res)=>{
+    reviewData=res.data;
+}).catch(()=>{});
+
+// const mockReview=[
+//     {
+//         id:0,
+//         userId:'user1',
+//         userNickname:'말랑박쥐',
+//         userCar:'클럽맨',
+//         rate:3,
+//         content:'이 차를 사느니 걸어다니겠습니다.'
+//     },
+//     {
+//         id:1,
+//         userId:'user2',
+//         userNickname:'딱딱구리',
+//         userCar:'5도어',
+//         rate:5,
+//         content:'이정도면 개 혜자임.'
+//     },
+//     {
+//         id:2,
+//         userId:'user3',
+//         userNickname:'콩송편은적폐',
+//         userCar:'컨트리맨',
+//         rate:5,
+//         content:'미니는 역시 돈 보고 타는 차는 아니네요. 이게 배고픈게 없으면 나가 스트라이프 파티 참여하면 얼마를 타고 좋아요.'
+//     },
+//     {
+//         id:3,
+//         userId:'user4',
+//         userNickname:'undeFined',
+//         userCar:'기타',
+//         rate:5,
+//         content:'역시 차는 롤스로이스죠~'
+//     }
+// ];
 
 const LifestyleReview=({visible})=>{
+    // review 페이지와 lifestyle 페이지에서 보여질 개수가 다르기 때문에 slice할 인덱스를 지정할 변수
     let slIdx;
-    visible==='part' ? slIdx=mockReview.length-2 : slIdx=0;
+
+    // 리뷰 데이터의 개수에 따라 리턴이 다름
+    if(!reviewData.length){
+        return <p>작성된 리뷰가 없습니다.</p>
+    }
+    else if(reviewData.length<=2){
+        // 리뷰가 2개 이하면 모두 출력
+        slIdx=0;
+    }
+    else{
+        // 리뷰가 2개 넘으면 lifestyle 페이지에선 2개만, 리뷰 페이지에선 모두 출력 
+        visible==='part' ? slIdx=reviewData.length-2 : slIdx=0;
+    }
     
     return (
         <ul className='review_list fs_sm'>
-            {mockReview.slice(slIdx).reverse().map((item)=>
+            {reviewData.slice(slIdx).reverse().map((item)=>
                 <li key={item.id} className='review_item'>
                     <div className='profile_img'><img src={profileImg} /></div>
                     <ul>
-                    <li><span className='ninckname'>{item.userId}</span> <span className='car'>{item.userCar}</span></li>
-                    <li className='mb_xsm'><span className='rate'>★★★★★</span></li>
+                    <li><span className='ninckname'>{item.nickname}</span> <span className='car'>{item.car}</span></li>
+                    <li className='mb_xsm'><span className='rate'>{'★'.repeat(item.rate)}</span></li>
                     <li><p className='content'>{item.content}</p></li>
                     </ul>
                 </li>

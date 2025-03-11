5661ddd2a419e804a999c89abce19c1d5ef0090e
duski96 <duski96@naver.com>
2025-03-12 03:43:56 +0900

    게시판별 DB 데이터 호출 라우터 추가

--- project02/server/routes/boardRouter.js
+++ project02/server/routes/boardRouter.js
@@ -2,9 +2,119 @@ import express from 'express';
 const router = express();
 import db from '../config/db.js';
 
+router.get('/review', (req, res)=>{
+    const sql='select * from review';
+    db.query(sql, [], (err, data)=>{
+        if(err){
+            res.status(500).send(err);
+        }
+        else if(!data.length){
+            res.status(204).send({message: '조회된 데이터가 없습니다.'});
+        }
+        else{
+            res.status(200).send(data);
+        }
+    });
+});
+
 router.get('/review_submit', (req, res)=>{
-    console.log(req);
-    res.send({r:'OK'});
+    const {userId, nickname, car, rate, content}=req.query;
+    
+    const sql='insert into review (user_id, nickname, car, rate, content) values (?, ?, ?, ?, ?)';
+
+    db.query(sql, [userId, nickname, car, Number(rate), content], (err, data)=>{
+        if(err){
+            res.status(500).send(err);
+        }
+        else{
+            res.status(200).send({message:'리뷰 작성이 완료되었습니다.'});
+        }
+    });
+});
+
+router.get('/post', (req, res)=>{
+    const getMeeting=()=>{
+        return new Promise((resolve, reject)=>{
+            const sql='select * from meeting';
+            db.query(sql, [], (err, data)=>{
+                if(err){
+                    reject(err);
+                }
+                else{
+                    resolve(data);
+                }
+            });
+        });
+    }
+
+    const getDriving=()=>{
+        return new Promise((resolve, reject)=>{
+            const sql='select * from driving';
+            db.query(sql, [], (err, data)=>{
+                if(err){
+                    reject(err);
+                }
+                else{
+                    resolve(data);
+                }
+            });
+        });
+    }
+
+    const getMaintenance=()=>{
+        return new Promise((resolve, reject)=>{
+            const sql='select * from maintenance';
+            db.query(sql, [], (err, data)=>{
+                if(err){
+                    reject(err);
+                }
+                else{
+                    resolve(data);
+                }
+            });
+        });
+    }
+
+    const getDefect=()=>{
+        return new Promise((resolve, reject)=>{
+            const sql='select * from defect';
+            db.query(sql, [], (err, data)=>{
+                if(err){
+                    reject(err);
+                }
+                else{
+                    resolve(data);
+                }
+            });
+        });
+    }
+    
+    Promise.all([getMeeting(), getDriving(), getMaintenance(), getDefect()])
+    .then(([meetingData, drivingData, maintenanceData, defectData])=>{
+        res.status(200).send({
+            meeting:meetingData,
+            driving:drivingData,
+            maintenance:maintenanceData,
+            defect:defectData
+        })
+    }).catch((err)=>{
+        res.status(500).send(err);
+    });
+});
+
+router.get('/post_submit', (req, res)=>{
+    const {userId, nickname, car, title, content, board}=req.query;
+    
+    const sql='insert into ? (user_id, nickname, car, title, content) values (?, ?, ?, ?, ?)';
+
+    db.query(sql, [board, userId, nickname, car, title, content], (err, data)=>{
+        if(err){
+            res.status(500).send(err);
+        }
+        else{
+            res.status(200).send({message:'글 작성이 완료되었습니다.'});
+        }
+    });
 });
 
 export default router;

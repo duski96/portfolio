c37c6e72ddc48bb4c8100cc6d3614a3962ce4896
duski96 <duski96@naver.com>
2025-03-13 22:21:02 +0900

    useEffect 추가, DB 로딩 완료 후 렌더링 진행

--- project02/src/components/sub/brand/LifestylePost.jsx
+++ project02/src/components/sub/brand/LifestylePost.jsx
@@ -1,88 +1,68 @@
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
+import Redirect from '../../Redirect';
 
-let postData;
-axios.get('/api/board/post').then((res)=>{
-    postData=res.data;
-    console.log(postData['meeting'].length);
-}).catch((err)=>{
-    console.log(err);
-});
-
-// const mockPost=[
-//     {
-//         id:0,
-//         userId:'user1',
-//         userNickname:'말랑박쥐',
-//         userCar:'클럽맨',
-//         title:'제 1회 부산 폭주 모임',
-//         content:'폭주합시다.'
-//     },
-//     {
-//         id:1,
-//         userId:'user2',
-//         userNickname:'딱딱구리',
-//         userCar:'5도어',
-//         title:'제 1회 부산 폭주 모임',
-//         content:'폭주합시다.'
-//     },
-//     {
-//         id:2,
-//         userId:'user3',
-//         userNickname:'콩송편은적폐',
-//         userCar:'컨트리맨',
-//         title:'제 1회 부산 폭주 모임',
-//         content:'폭주합시다.'
-//     },
-//     {
-//         id:3,
-//         userId:'user4',
-//         userNickname:'undeFined',
-//         userCar:'기타',
-//         title:'제 1회 부산 폭주 모임',
-//         content:'폭주합시다.'
-//     }
-// ];
-
-// postData는 review와 달리 네 개의 테이블을 한 번에 불러옴
-// tableName 파라미터에 따라 몇 번째 테이블을 불러올지 정해짐
-const LifestylePost=({visible, tableName})=>{
-    
-    // lifestyle 페이지가 아닌 해당 게시판 페이지라면 useParams로 불러올 데이터 구분
-    const boardName=useParams().boardName;
-
-    // lifestyle 페이지에선 tableName, 게시판 페이지에선 boardName이 속성으로 사용됨
-    let attr;
-    tableName ? attr=tableName : attr=boardName;
-
-    //post 페이지와 lifestyle 페이지에서 보여질 개수가 다르기 때문에 slice할 인덱스를 지정할 변수
-    let slIdx;
-
-    // 작성글 데이터의 개수에 따라 리턴이 다름
-    if(!postData[attr].length){
-        return <p>작성된 글이 없습니다.</p>
-    }
-    else if(postData[attr].length<=3){
-        // 작성글이 3개 이하면 모두 출력
-        slIdx=0;
-    }
-    else{
-        // 작성글이 2개 넘으면 lifestyle 페이지에선 3개만, 리뷰 페이지에선 모두 출력 
-        visible==='part' ? slIdx=postData[attr].length-3 : slIdx=0;
-    }
-
+const returnMarkup=(boardName, boardList, sliceIndex)=>{
+    // boardName : 불러와야 할 게시판 이름,
+    // boardList : 해당 게시판 리스트,
+    // sliceIndex : 페이지에 따라 리스트의 개수를 다르게 출력
     return (
         <ul className='post_list fs_sm'>
-            {postData[attr].slice(slIdx).reverse().map((item)=>
+            {boardList.slice(sliceIndex).reverse().map((item)=>
                 <li key={item.id} className='post_item'>
-                    <ul>
-                        <li className='mb_sm'><span className='title fs_md'>{item.title}</span></li>
-                        <li><span className='ninckname'>{item.nickname}</span> <span className='car'>{item.car}</span></li>
-                    </ul>
+                    <Link to={`/brand/lifestyle/${boardName}/${item.id}`} state={item}>
+                        <ul>
+                            <li className='mb_sm'><span className='title fs_md'>{item.title}</span></li>
+                            <li><span className='ninckname'>{item.nickname}</span> <span className='car'>{item.car}</span></li>
+                        </ul>
+                    </Link>
                 </li>
             )}
         </ul>
     );
+}
+
+const LifestylePost=({visible, tableName})=>{
+    // lifestyle 페이지면 tableName으로,
+    // 해당 게시판 페이지라면 useParams로 게시판 이름 구분
+    let boardName=useParams().boardName || tableName;
+
+    // 현재 게시판의 데이터를 저장할 state
+    const [boardList, setBoardList]=useState(null);
+
+    // 로딩 실패 여부 : 성공 true, 실패 false
+    const [loadSuccess, setLoadSuccess]=useState(true);
+
+    // DB에서 데이터를 불러온 후 다시 렌더링
+    useEffect(()=>{
+        axios.get(`/api/board/${boardName}`).then((res)=>{
+            const loadData=res.data;
+            setBoardList(loadData);
+        }).catch(()=>{
+            setLoadSuccess(false);
+        });
+    }, []);
+
+    if(!loadSuccess)
+        return <Redirect message={'데이터 로딩에 실패했습니다.'} />
+
+    if(!boardList){
+        return <p>작성된 글이 없습니다.</p>
+    }
+    else if(visible==='part'){
+        // lifestyle 페이지에서 보여질 리스트
+        if(boardList.length-3){
+            return returnMarkup(boardName, boardList, 0);
+        }
+        else{
+            return returnMarkup(boardName, boardList, boardList.length-3);
+        }
+    }
+    else{
+        // 해당 게시판 페이지에서 보여질 리스트
+        return returnMarkup(boardName, boardList, 0);
+    }
 }
 
 export default LifestylePost;

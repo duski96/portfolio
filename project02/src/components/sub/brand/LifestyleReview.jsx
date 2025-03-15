ba66b3f5ff953bf355308a4cbd9c2d705937d8af
duski96 <duski96@naver.com>
2025-03-15 16:33:11 +0900

    axios 통신 코드 수정, 삭제 버튼 추가

--- project02/src/components/sub/brand/LifestyleReview.jsx
+++ project02/src/components/sub/brand/LifestyleReview.jsx
@@ -1,38 +1,50 @@
 import profileImg from '../../../assets/sub/profile_default.jpg';
+import { useState, useEffect, useContext } from 'react';
+import { LoginUserInfoContext } from '../../../App';
 import axios from 'axios';
 
-let reviewData;
-axios.get('/api/board/review').then((res)=>{
-    reviewData=res.data;
-}).catch(()=>{});
-
 const LifestyleReview=({visible})=>{
+    const {loginUserInfo}=useContext(LoginUserInfoContext);
+
+    // 리뷰 데이터를 저장할 state
+    const [reviewData, setReviewData]=useState();
+
     // review 페이지와 lifestyle 페이지에서 보여질 개수가 다르기 때문에 slice할 인덱스를 지정할 변수
-    let slIdx;
+    let slIdx=0;
+
+    //DB에서 리뷰 테이블 데이터 로딩 후 다시 렌더링
+    useEffect(()=>{
+        axios.get('/api/board/review').then((res)=>{
+            setReviewData(res.data);
+        }).catch(()=>{
+            alert('로딩 중 오류가 발생했습니다!');
+        });
+    }, []);
 
     // 리뷰 데이터의 개수에 따라 리턴이 다름
-    if(!reviewData.length){
+    if(!reviewData){
         return <p>작성된 리뷰가 없습니다.</p>
     }
-    else if(reviewData.length<=2){
-        // 리뷰가 2개 이하면 모두 출력
-        slIdx=0;
-    }
     else{
-        // 리뷰가 2개 넘으면 lifestyle 페이지에선 2개만, 리뷰 페이지에선 모두 출력 
-        visible==='part' ? slIdx=reviewData.length-2 : slIdx=0;
+        // lifestyle 페이지에선 2개만, 리뷰 페이지에선 모두 출력 
+        visible==='part' ? slIdx=reviewData.length-2 : slIdx;
     }
     
     return (
         <ul className='review_list fs_sm'>
             {reviewData.slice(slIdx).reverse().map((item)=>
                 <li key={item.id} className='review_item'>
-                    <div className='profile_img'><img src={profileImg} /></div>
-                    <ul>
-                    <li><span className='ninckname'>{item.nickname}</span> <span className='car'>{item.car}</span></li>
-                    <li className='mb_xsm'><span className='rate'>{'★'.repeat(item.rate)}</span></li>
-                    <li><p className='content'>{item.content}</p></li>
+                    <div className='profile_img'>
+                        <img src={profileImg} />
+                    </div>
+                    <ul className='info'>
+                        <li><span className='ninckname'>{item.nickname}</span> <span className='car'>{item.car}</span></li>
+                        <li className='mb_xsm'><span className='rate'>{'★'.repeat(item.rate)}</span></li>
+                        <li><p className='content'>{item.content}</p></li>
                     </ul>
+                    <div className={`button_area ${item.user_id===loginUserInfo.userId ? 'visible' : ''}`}>
+                        <button type='button' className='delete fs_sm'>삭제</button>
+                    </div>
                 </li>
             )}
         </ul>

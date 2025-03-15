3fbd40b1f973ce3b1419af762cdd3e446bd0933d
duski96 <duski96@naver.com>
2025-03-15 16:37:34 +0900

    분리된 마크업 재통합

--- project02/src/components/sub/brand/LifestylePost.jsx
+++ project02/src/components/sub/brand/LifestylePost.jsx
@@ -3,26 +3,6 @@ import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 import Redirect from '../../Redirect';
 
-const returnMarkup=(boardName, boardList, sliceIndex)=>{
-    // boardName : 불러와야 할 게시판 이름,
-    // boardList : 해당 게시판 리스트,
-    // sliceIndex : 페이지에 따라 리스트의 개수를 다르게 출력
-    return (
-        <ul className='post_list fs_sm'>
-            {boardList.slice(sliceIndex).reverse().map((item)=>
-                <li key={item.id} className='post_item'>
-                    <Link to={`/brand/lifestyle/${boardName}/${item.id}`} state={item}>
-                        <ul>
-                            <li className='mb_sm'><span className='title fs_md'>{item.title}</span></li>
-                            <li><span className='ninckname'>{item.nickname}</span> <span className='car'>{item.car}</span></li>
-                        </ul>
-                    </Link>
-                </li>
-            )}
-        </ul>
-    );
-}
-
 const LifestylePost=({visible, tableName})=>{
     // lifestyle 페이지면 tableName으로,
     // 해당 게시판 페이지라면 useParams로 게시판 이름 구분
@@ -47,22 +27,37 @@ const LifestylePost=({visible, tableName})=>{
     if(!loadSuccess)
         return <Redirect message={'데이터 로딩에 실패했습니다.'} />
 
+
+    let slIdx=0; // 페이지에 따라 출력될 리스트의 개수가 다르기 때문에 slice할 인덱스 지정
+
     if(!boardList){
         return <p>작성된 글이 없습니다.</p>
     }
     else if(visible==='part'){
         // lifestyle 페이지에서 보여질 리스트
-        if(boardList.length-3){
-            return returnMarkup(boardName, boardList, 0);
-        }
-        else{
-            return returnMarkup(boardName, boardList, boardList.length-3);
+        // 작성된 글이 3개 이하면 모든 글 출력
+        if(boardList.length-3 > 0){
+            slIdx=boardList.length-3;
         }
     }
-    else{
-        // 해당 게시판 페이지에서 보여질 리스트
-        return returnMarkup(boardName, boardList, 0);
-    }
+
+    // boardName : 불러와야 할 게시판 이름,
+    // boardList : 해당 게시판 리스트,
+    // slIdx : 페이지에 따라 리스트의 개수를 다르게 출력
+    return (
+        <ul className='post_list fs_sm'>
+            {boardList.slice(slIdx).reverse().map((item)=>
+                <li key={item.id} className='post_item'>
+                    <Link to={`/brand/lifestyle/${boardName}/${item.id}`} state={item}>
+                        <ul>
+                            <li className='mb_sm'><span className='title fs_md'>{item.title}</span></li>
+                            <li><span className='ninckname'>{item.nickname}</span> <span className='car'>{item.car}</span></li>
+                        </ul>
+                    </Link>
+                </li>
+            )}
+        </ul>
+    );
 }
 
 export default LifestylePost;

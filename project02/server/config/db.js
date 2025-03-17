c34b070cd0d6092f6c957418e1341dc3aed116b6
duski96 <duski96@naver.com>
2025-03-18 02:40:46 +0900

    console.log 제거

--- project02/server/config/db.js
+++ project02/server/config/db.js
@@ -5,7 +5,7 @@ import mysql from 'mysql2';
 import dotenv from 'dotenv';
 import path from 'path';
 dotenv.config({path:path.resolve(process.cwd(), '../.env')});
- 
+
 const db = mysql.createPool({
     host:process.env.MYSQLHOST,
     user:process.env.MYSQLUSER,

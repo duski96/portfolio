10333141a9bb5c90f828fb8e57391f3d612336d3
duski96 <duski96@naver.com>
2025-03-18 02:41:36 +0900

    target 경로 수정

--- project02/vite.config.js
+++ project02/vite.config.js
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({mode})=>{
-  //const env=loadEnv(mode, process.cwd())
+  const env=loadEnv(mode, process.cwd())
 
   return {
     plugins: [react()],
     server:{
       proxy:{
         '/api': { // axios라이브러리 등으로 http 요청인데 api로 시작하면
-          target: 'https://portfolio-production-790b.up.railway.app', // 이쪽 주소로 맵핑하여 백그라운드로 보냄.
+          target: 'https://portfolio-production-790b.up.railway.app/', // 이쪽 주소로 맵핑하여 백그라운드로 보냄.
           changeOrigin: true, // cors 에러 방지
           secure: false
         }

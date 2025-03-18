38e2c354798681ece6d94f9781bd48fcb1512a82
duski96 <duski96@naver.com>
2025-03-18 17:48:07 +0900

    railway db 서버로 타겟 설정

--- project02/vite.config.js
+++ project02/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({mode})=>{
-  const env=loadEnv(mode, process.cwd())
+  // const env=loadEnv(mode, process.cwd())
 
   return {
     plugins: [react()],
@@ -18,6 +18,3 @@ export default defineConfig(({mode})=>{
     }
   }
 });
-
-
-//https://portfolio-production-790b.up.railway.app

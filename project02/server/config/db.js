40f7bca6b7f7c3b5ffc88a7cd15da563ad90b428
duski96 <duski96@naver.com>
2025-03-18 11:02:01 +0900

    포트번호 추가

--- project02/server/config/db.js
+++ project02/server/config/db.js
@@ -10,7 +10,8 @@ const db = mysql.createPool({
     host:process.env.MYSQLHOST,
     user:process.env.MYSQLUSER,
     password:process.env.MYSQLPASSWORD,
-    database:process.env.MYSQLDATABASE
+    database:process.env.MYSQLDATABASE,
+    port:process.env.MYSQLPORT || 3306
 });
 
 export default db;

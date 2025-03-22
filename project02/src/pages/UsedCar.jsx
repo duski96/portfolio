7f880bc3c4344e0e62cbbe348c5db8ba9afc1cb0
duski96 <duski96@naver.com>
2025-03-23 02:31:21 +0900

    인증중고차, LOYALTY 페이지 추가

--- project02/src/pages/UsedCar.jsx
+++ project02/src/pages/UsedCar.jsx
@@ -1,24 +1,39 @@
 import { useParams } from "react-router-dom";
-
 import Header from "../components/Header";
 import SubMain from "../components/sub/SubMain";
-import TempPage from "../components/sub/TempPage"
+import UsedCarContent from "../components/sub/usedcar/UsedCarContent";
 import QuickMenu from "../components/QuickMenu";
 import Footer from "../components/Footer";
-
+import Redirect from "../components/Redirect";
 import subMainBg from '../assets/sub/used_car_main.jpg';
 
 const UsedCar=()=>{
-    const params=useParams();
-    const paramsName=params.subPageName;
+    const paramsName=useParams().subPageName;
+
+    const returnExplanationTxt=()=>{
+        switch(paramsName){
+            case 'certified' : return '인증중고차';
+            case 'loyalty' : return 'LOYALTY PROGRAM';
+            case 'sales' : return '내차 판매 문의';
+            case 'desired' : return '희망차량 등록 알림';
+            default : break;
+        }
+    }
+
+    const explanationTxt=returnExplanationTxt();
 
     return (
+        ['certified', 'loyalty', 'sales', 'desired'].includes(paramsName) ?
         <>
             <Header isActive={true} />
-            <SubMain title={'UsedCar'} explanation={`${paramsName.toUpperCase()} 페이지 입니다.`} background={subMainBg} addClassName={'UsedCar'} />
-            <TempPage paramsName={paramsName} />
+            <SubMain title={'UsedCar'} explanation={explanationTxt} background={subMainBg} addClassName={'UsedCar'} />
+            <UsedCarContent paramsName={paramsName} />
             <QuickMenu />
             <Footer />
+        </>
+        :
+        <>
+            <Redirect message={`해당 페이지는 존재하지 않습니다.`} />
         </>
     );
 }

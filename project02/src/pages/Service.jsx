c5ac867105e4cdfb44a2c5da787d273797e1f969
duski96 <duski96@naver.com>
2025-03-23 02:32:23 +0900

    Service 페이지 전체 추가

--- project02/src/pages/Service.jsx
+++ project02/src/pages/Service.jsx
@@ -1,22 +1,19 @@
 import { useParams } from "react-router-dom";
-
 import Header from "../components/Header";
 import SubMain from "../components/sub/SubMain";
-import TempPage from "../components/sub/TempPage"
+import ServiceContent from "../components/sub/service/ServiceContent";
 import QuickMenu from "../components/QuickMenu";
 import Footer from "../components/Footer";
-
 import subMainBg from '../assets/sub/service_main.jpg';
 
 const Service=()=>{
-    const params=useParams();
-    const paramsName=params.subPageName;
+    const paramsName=useParams().subPageName;
 
     return (
         <>
             <Header isActive={true} />
             <SubMain title={'Service'} explanation={`${paramsName.toUpperCase()} 페이지 입니다.`} background={subMainBg} addClassName={'Service'} />
-            <TempPage paramsName={paramsName} />
+            <ServiceContent paramsName={paramsName} />
             <QuickMenu />
             <Footer />
         </>

068dd65fd8a8c2ca3f2777645a22f6142fd2951f
duski96 <duski96@naver.com>
2025-03-23 02:34:20 +0900

    미완성 페이지 파라미터 변경

--- project02/src/components/sub/brand/BrandContent.jsx
+++ project02/src/components/sub/brand/BrandContent.jsx
@@ -2,11 +2,12 @@ import './BrandContent.css';
 import News from './News.jsx';
 import Inside from './Inside.jsx';
 import Lifestyle from './Lifestyle.jsx';
+import TempPage from '../TempPage.jsx';
 
 const BrandContent=({paramsName})=>{
     switch(paramsName){
-        case 'news' : return <News />
-        case 'inside' : return <Inside />
+        case 'news' : return <TempPage curPage={'NEWS'} />
+        case 'inside' : return <TempPage curPage={'INSIDE MINI'} />
         case 'lifestyle' : return <Lifestyle />
         default : break;
     }

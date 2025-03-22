2485a6357c90688a6bdf6c94a1976a00213b8262
duski96 <duski96@naver.com>
2025-03-23 02:34:59 +0900

    파라미터 변경

--- project02/src/components/sub/TempPage.jsx
+++ project02/src/components/sub/TempPage.jsx
@@ -1,11 +1,11 @@
 import './TempPage.css';
 
-const TempPage=({paramsName})=>{
+const TempPage=({curPage})=>{
     return (
         <section className="TempPage">
             <div className='inner_1280'>
                 <div className='temp_area'>
-                    <p className="fs_lg">{paramsName} 페이지는 <br />준비중입니다.</p>
+                    <p className="fs_lg"><b>{curPage}</b> 페이지는 <br />준비중입니다.</p>
                 </div>
             </div>
         </section>

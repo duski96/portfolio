c86fde4c67b5db8d1f20dd016f33c78c9f2e2b68
duski96 <duski96@naver.com>
2025-03-23 02:35:59 +0900

    pagination 추가 (progressbar)

--- project02/src/components/main/ForSaleList.jsx
+++ project02/src/components/main/ForSaleList.jsx
@@ -1,16 +1,14 @@
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import { Link } from 'react-router-dom';
-
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
 import 'swiper/css';
-
-import heart from './../../assets/main/forsale_heart.png';
-
+import 'swiper/css/pagination';
 import { MockDataContext, MockDataDispatchContext } from '../../App';
-
 import { getMiniImage } from '../../util/get-mini-image.js';
 import { replaceFuelTxt } from '../../util/replace-fuel-txt.js';
 import { replaceSpotTxt } from '../../util/replace-spot-txt.js';
+import heart from './../../assets/main/forsale_heart.png';
 
 const ForSaleList = ({ filteredList }) => {
     const {interestId}=useContext(MockDataContext);
@@ -21,41 +19,46 @@ const ForSaleList = ({ filteredList }) => {
             <div className='filtered_list'>
                 {filteredList.length ?
                     (
-                        <Swiper
-                            spaceBetween={32}
-                            slidesPerView={4}
-                        >
-                            {filteredList.map((item) => (<SwiperSlide key={item.id}>
-                                <div className='info fs_sm NotoSansKR'>
-                                    <div className='function'>
-                                        <button type="button" onClick={()=>{getInterestId(item.id)}} className={interestId.includes(item.id) ? 'active' : ''}><img src={heart} alt='관심 매물 등록 아이콘' /></button>
+                        <>
+                            <Swiper
+                                modules={[Pagination]}
+                                spaceBetween={32}
+                                slidesPerView={4}
+                                pagination={{clickable:true, type:'progressbar'}}
+                            >
+                                {filteredList.map((item) => (<SwiperSlide key={item.id}>
+                                    <div className='info fs_sm NotoSansKR'>
+                                        <div className='function'>
+                                            <button type="button" onClick={()=>{getInterestId(item.id)}} className={interestId.includes(item.id) ? 'active' : ''}><img src={heart} alt='관심 매물 등록 아이콘' /></button>
+                                        </div>
+                                        <Link to={`/product/${item.id}`}>
+                                            <div className='picture'>
+                                                <img src={getMiniImage(item.id)} alt={item.series} className='model' />
+                                            </div>
+                                            <div className='txt'>
+                                                <h4 className='fs_md'><b>{item.series.toUpperCase()}</b></h4>
+                                                <p className='fs_md mb_sm'><b>{item.price} 만원</b></p>
+                                                <div className='flex j-sb mb_xsm'>
+                                                    <p>주행거리</p>
+                                                    <p>{item.mileage}km</p>
+                                                </div>
+                                                <div className='flex j-sb mb_xsm'>
+                                                    <p>연식</p>
+                                                    <p>{item.year}</p>
+                                                </div>
+                                                <div className='flex j-sb mb_sm'>
+                                                    <p>연료</p>
+                                                    <p>{replaceFuelTxt(item.fuel)}</p>
+                                                </div>
+                                                <hr className='mb_xsm' />
+                                                <p>{replaceSpotTxt(item.spot)}</p>
+                                            </div>
+                                        </Link>
                                     </div>
-                                    <Link to={`/product/${item.id}`}>
-                                        <div className='picture'>
-                                            <img src={getMiniImage(item.id)} alt={item.series} className='model' />
-                                        </div>
-                                        <div className='txt'>
-                                            <h4 className='fs_md'><b>{item.series.toUpperCase()}</b></h4>
-                                            <p className='fs_md mb_sm'><b>{item.price} 만원</b></p>
-                                            <div className='flex j-sb mb_xsm'>
-                                                <p>주행거리</p>
-                                                <p>{item.mileage}km</p>
-                                            </div>
-                                            <div className='flex j-sb mb_xsm'>
-                                                <p>연식</p>
-                                                <p>{item.year}</p>
-                                            </div>
-                                            <div className='flex j-sb mb_sm'>
-                                                <p>연료</p>
-                                                <p>{replaceFuelTxt(item.fuel)}</p>
-                                            </div>
-                                            <hr className='mb_xsm' />
-                                            <p>{replaceSpotTxt(item.spot)}</p>
-                                        </div>
-                                    </Link>
-                                </div>
-                            </SwiperSlide>))}
-                        </Swiper>
+                                </SwiperSlide>))}
+                            </Swiper>
+                        </>
+                        
                     )
                     : (
                         <div className='no_result'>

d2e99719f5eb11674e9284be612b12b5411c5df3
duski96 <duski96@naver.com>
2025-03-25 19:04:05 +0900

    반응형 관련 수정, 삭제 버튼 활성화

--- project02/src/components/sub/brand/LifestyleReview.jsx
+++ project02/src/components/sub/brand/LifestyleReview.jsx
@@ -12,13 +12,18 @@ const LifestyleReview=({visible})=>{
     // review 페이지와 lifestyle 페이지에서 보여질 개수가 다르기 때문에 slice할 인덱스를 지정할 변수
     let slIdx=0;
 
-    //DB에서 리뷰 테이블 데이터 로딩 후 다시 렌더링
-    useEffect(()=>{
+    // 리스트 렌더링 함수
+    const ListLoading=()=>{
         axios.get('/api/board/review').then((res)=>{
             setReviewData(res.data);
         }).catch(()=>{
             alert('DB 연결에 실패했습니다.');
         });
+    }
+
+    //DB에서 리뷰 테이블 데이터 로딩 후 다시 렌더링
+    useEffect(()=>{
+        ListLoading();
     }, []);
 
     // 리뷰 데이터의 개수에 따라 리턴이 다름
@@ -29,21 +34,45 @@ const LifestyleReview=({visible})=>{
         // lifestyle 페이지에선 2개만, 리뷰 페이지에선 모두 출력 
         visible==='part' ? slIdx=reviewData.length-2 : slIdx=0;
     }
+
+    
+    // 리뷰 삭제 버튼
+    const onClickDelete=(e)=>{
+        // 삭제 전 최종 확인
+        if(window.confirm('리뷰를 삭제하시겠습니까?')){
+            // 확인 시 게시판 삭제 api 호출
+            axios.get('/api/board/delete', {params:{id:e.target.dataset.itemId, userId:e.target.dataset.userId, board:'review'}});
+
+            // 삭제 알림
+            alert('리뷰가 삭제되었습니다.');
+
+            // 삭제 후 리뷰 리스트 다시 렌더링. 비동기 호출 방지를 위해 리스트 삭제 후 1초 후 실행
+            setTimeout(()=>{
+                ListLoading();
+            }, 1000);
+        }
+        else{
+            // 취소 시 클릭 이벤트 종료
+            return;
+        }
+    }
     
     return (
         <ul className='review_list fs_sm'>
             {reviewData.slice(slIdx).reverse().map((item)=>
-                <li key={item.id} className='review_item'>
-                    <div className='profile_img'>
-                        <img src={profileImg} />
+                <li key={item.id} className='review_item mb_md'>
+                    <div className='review_info'>
+                        <div className='profile_img'>
+                            <img src={profileImg} />
+                        </div>
+                        <ul className='info'>
+                            <li><span className='ninckname'>{item.nickname}</span> <span className='car'>{item.car}</span></li>
+                            <li className='mb_xsm'><span className='rate'>{'★'.repeat(item.rate)}</span></li>
+                            <li><p className='content'>{item.content}</p></li>
+                        </ul>
                     </div>
-                    <ul className='info'>
-                        <li><span className='ninckname'>{item.nickname}</span> <span className='car'>{item.car}</span></li>
-                        <li className='mb_xsm'><span className='rate'>{'★'.repeat(item.rate)}</span></li>
-                        <li><p className='content'>{item.content}</p></li>
-                    </ul>
                     <div className={`button_area ${item.user_id===loginUserInfo.userId ? 'visible' : ''}`}>
-                        <button type='button' className='delete fs_sm'>삭제</button>
+                        <button type='button' className='delete fs_sm' onClick={onClickDelete} data-item-id={item.id} data-user-id={item.user_id}>삭제</button>
                     </div>
                 </li>
             )}

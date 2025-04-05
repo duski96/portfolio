fe648d0fa7a11d6dce4c6e46c7a66220efd2acc5
duski96 <duski96@naver.com>
2025-04-06 03:12:57 +0900

    홈화면 검색 영역 추가

---
+++ project03/src/components/main/SearchArea.tsx
@@ -0,0 +1,13 @@
+import SearchFunc from "../SearchFunc";
+
+const SearchArea=()=>{
+    return (
+        <section className="SearchArea">
+            <div className="inner_1000">
+                <SearchFunc />
+            </div>
+        </section>
+    );
+}
+
+export default SearchArea;

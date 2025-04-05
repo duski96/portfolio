76f2d078a51b16d3199b298d1ad4681d0f3979c2
duski96 <duski96@naver.com>
2025-04-06 03:09:02 +0900

    api 추가 로딩 및 아이콘 관련 수정

--- project03/src/components/info/CurrentWeather.tsx
+++ project03/src/components/info/CurrentWeather.tsx
@@ -1,40 +1,37 @@
-import { useEffect } from "react";
+import './Info.css';
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { LoadedDataType, useLoadedDataContext, useWeatherInfoContext } from "../../App";
 import beachInfo from '../../assets/beachInfo.json';
+import { getUltraSrtFcstApiUrl, getWhBuoyApiUrl, getTwBuoyApiUrl } from '../../util/getApiUrl';
+import { getPTY, getSKY, getVEC } from '../../util/getAttrValue';
+import { getWeatherIcon } from '../../util/getWeatherIcon';
+import waveIcon from '../../assets/wave_icon.png';
+import windIcon from '../../assets/wind_icon.png';
 
-const Title=()=>{
+const CurrentWeather=()=>{
     // 현재 페이지 url 파라미터 === 해수욕장 고유번호
     const param=Number(useParams().id);
 
     // null 타입을 받아올 경우를 대비해 useContext 대신 custom Hook으로 받아옴
     const {loadedData, setLoadedData}=useLoadedDataContext();
 
-    // api url에 사용할 변수 생성
-    const year=new Date().getFullYear().toString();
-    const month=`0${new Date().getMonth()+1}`.slice(-2);
-    const date=`0${new Date().getDate()}`.slice(-2);
-    const baseDate=`${year}${month}${date}`;
-
-    // api의 base_time에 여유를 두기 위해 1시간 빼기
-    const hour=`0${new Date().getHours()-1}`.slice(-2);
-    const baseTime=`${hour}00`;
-
     // api 호출 후 loadedData에 저장
     useEffect(()=>{
-        const apiUrl=`https://apis.data.go.kr/1360000/BeachInfoservice/getUltraSrtFcstBeach?serviceKey=tSschjwEmaSA4wuIPOHxP00%2B0IJG7SAQ9pw4W9FPOFH79MKUK0hGrrp0byPhCafekK4dyrKqLYnZKtVKUnpbuA%3D%3D&numOfRows=60&pageNo=1&dataType=JSON&base_date=${baseDate}&base_time=${baseTime}&beach_num=${param}`;
-
+        // api url 반환 함수 호출
+        const apiUrl=getUltraSrtFcstApiUrl(param);
+        // api 호출
         axios.get(apiUrl).then(
             (res)=>{
                 const result: LoadedDataType[]=res.data.response.body.items.item;
                 setLoadedData(result);
             }
-        ).catch();
+        ).catch((err: string)=>{throw new Error (err);});
     }, [param]);
 
     // 날씨 정보(속성 값) 불러옴
-    const {weatherInfo, setWeatherInfo}=useWeatherInfoContext();
+    const {weatherInfo}=useWeatherInfoContext();
 
     // 날씨 속성 - 기온, 강수량, 하늘상태, 동서바람성분, 남북바람성분, 습도, 강수형태, 풍향, 풍속
     const attrArr=['T1H', 'RN1', 'SKY', 'UUU', 'VVV', 'REH', 'PTY', 'VEC', 'WSD'];
@@ -68,41 +65,66 @@ const Title=()=>{
         windSpd:weatherInfo.WSD[0],
     }
 
-    // 강수형태 반환
-    const returnPTY=(value: string)=>{
-        switch(value){
-            case '0' : return '강수 없음';
-            case '1' : return '비';
-            case '2' : return '비 / 눈';
-            case '3' : return '눈';
-            case '4' : return '소나기';
-            case '5' : return '빗방울';
-            case '6' : return '빗방울 / 눈날림';
-            case '7' : return '눈날림';
-            default : return '확인 불가';
-        }
-    }
+    // 수온과 파고의 state
+    const [waveInfo, setWaveInfo]=useState({
+        tempWater:'string',
+        waveHeight:'string'
+    });
 
-    // 하늘상태 반환
-    const returnSKY=(value: string)=>{
-        switch(value){
-            case '1' : return '맑음';
-            case '3' : return '구름 많음';
-            case '4' : return '흐림';
-            default : return '확인 불가';
-        }
-    }
+    useEffect(()=>{
+        // 수온 api 호출 후 업데이트
+        const twApiUrl=getTwBuoyApiUrl(param);
+        axios.get(twApiUrl).then((res)=>{
+            const tw=res.data.response.body.items.item[0].tw;
+            setWaveInfo((waveInfo)=>{return {...waveInfo, tempWater:tw}});
+        }).catch((err)=>{
+            console.log(err);
+            return err;
+        });
+
+        // 파고 api 호출 후 업데이트
+        const whApiUrl=getWhBuoyApiUrl(param);
+        axios.get(whApiUrl).then((res)=>{
+            const wh=res.data.response.body.items.item[0].wh;
+            setWaveInfo((waveInfo)=>{return {...waveInfo, waveHeight:wh}});
+        }).catch((err)=>{
+            console.log(err);
+            return err;
+        });
+    }, [param]);
+
+    // 날씨 아이콘 출력에 필요한 변수
+    const currentHour=new Date().getHours();
 
     return (
-        <section className='Info Title'>
-            <div className='inner_1000'>
-                <h2>{currentBeachInfo.name}</h2>
-                <p>{currentBeachInfo.temp}℃</p>
-                <p>습도 {currentBeachInfo.humid}%</p>
-                <p>{returnPTY(currentBeachInfo.precType)} | {returnSKY(currentBeachInfo.sky)}</p>
+        <article className='Info CurrentWeather'>
+            <h2 className='fs_lg'>{currentBeachInfo.name}</h2>
+            <div className='weather'>
+                <img src={getWeatherIcon(currentBeachInfo.precType, currentBeachInfo.sky, currentHour)} alt="날씨 아이콘" />
+                <div className='txt'>
+                    <p className='temp fs_xlg'>{currentBeachInfo.temp}℃</p>
+                    <p className='humidity fs_md'>습도 <b>{currentBeachInfo.humid}%</b></p>
+                    <p className='state fs_md'>{getPTY(currentBeachInfo.precType)} | {getSKY(currentBeachInfo.sky)}</p>
+                </div>
             </div>
-        </section>
+            <ul className='add_info fs_md'>
+                <li>
+                    <img src={waveIcon} alt="파도 아이콘" />
+                    <p>
+                        수온: <b>{waveInfo.tempWater}℃</b> <br />
+                        파고: <b>{waveInfo.waveHeight}M</b>
+                    </p>
+                </li>
+                <li>
+                    <img src={windIcon} alt="바람 아이콘" />
+                    <p>
+                        풍향: <b>{getVEC(currentBeachInfo.windDir)} {currentBeachInfo.windDir}º</b> <br />
+                        풍속: <b>{currentBeachInfo.windSpd}m/s</b>
+                    </p>
+                </li>
+            </ul>
+        </article>
     );
 }
 
-export default Title;
+export default CurrentWeather;

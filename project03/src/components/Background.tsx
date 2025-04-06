4516faa31107c17904e27cf8944f88e975bf74e3
duski96 <duski96@naver.com>
2025-04-07 02:07:38 +0900

    배경 동영상 적용

--- project03/src/components/Background.tsx
+++ project03/src/components/Background.tsx
@@ -1,7 +1,27 @@
 import './Background.css';
+import backgroundDay from '../assets/background_day.mp4';
+import backgroundNight from '../assets/background_night.mp4';
 
 const Background=()=>{
-    return <div className='Background'></div>
+    const bgTime=new Date().getHours();
+
+    if(bgTime>=6 && bgTime<18){
+        <div className='Background'>
+            <video className='vid' autoPlay={true} loop muted playsInline>
+                <source src={backgroundDay} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+        </div>
+    }
+
+    return (
+        <div className='Background'>
+            <video className='vid' autoPlay={true} loop muted playsInline>
+                <source src={backgroundNight} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+        </div>
+    );
 }
 
 export default Background;

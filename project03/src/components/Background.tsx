d73e4f350f3a2495b4d7ecd7e354b4be8f623f06
duski96 <duski96@naver.com>
2025-04-07 11:22:56 +0900

    배경 반영 코드 수정

--- project03/src/components/Background.tsx
+++ project03/src/components/Background.tsx
@@ -3,21 +3,12 @@ import backgroundDay from '../assets/background_day.mp4';
 import backgroundNight from '../assets/background_night.mp4';
 
 const Background=()=>{
-    const bgTime=new Date().getHours();
-
-    if(bgTime>=6 && bgTime<18){
-        <div className='Background'>
-            <video className='vid' autoPlay={true} loop muted playsInline>
-                <source src={backgroundDay} type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
-        </div>
-    }
+    const time=new Date().getHours();
 
     return (
         <div className='Background'>
             <video className='vid' autoPlay={true} loop muted playsInline>
-                <source src={backgroundNight} type="video/mp4" />
+                <source src={time>=6 && time<18 ? backgroundDay : backgroundNight} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
         </div>

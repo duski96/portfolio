96950446a18c3805578bef1c0eac2779a41e9eed
duski96 <duski96@naver.com>
2025-04-08 11:37:25 +0900

    useLocation().state 유무에 따른 버튼 동작 조정

--- project03/src/components/info/FuncArea.tsx
+++ project03/src/components/info/FuncArea.tsx
@@ -1,15 +1,25 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import homeIcon from '../../assets/home.png';
 import undoIcon from '../../assets/undo.png';
 import SearchFunc from '../SearchFunc';
 
 const FuncArea=()=>{
+    // 검색 목록을 클릭했을 때 받아온 state
+    const state=useLocation().state;
+
+    // 버튼 동작 경로 설정
     const nav=useNavigate();
     const onClickHome=()=>{
         nav('/');
     }
     const onClickUndo=()=>{
-        nav(-1);
+        if(state){
+            nav(-1);
+        }
+        else{
+            // 홈 화면을 거치지 않고 접속했을 경우 뒤로가기 버튼 클릭 시 홈으로
+            nav('/');
+        }
     }
 
     const time=new Date().getHours();

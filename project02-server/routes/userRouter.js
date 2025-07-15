7dcf3e1a0bddd6409e7a7c7c834cce31a4b421e0
duski96 <duski96@naver.com>
2025-07-15 23:18:04 +0900

    분리된 server 폴더 업로드

---
+++ project02-server/routes/userRouter.js
@@ -0,0 +1,33 @@
+import db from '../config/db.js';
+import express from 'express';
+
+const router = express();
+
+router.post('/profile_update', (req, res)=>{
+    const {userId, newPw, newCar}=req.body.updateSubmit;
+    let dataParams=[];
+    let sql;
+    if(newPw && newCar){
+        sql='update users set user_pw=?, car=? where user_id=?';
+        dataParams.push(newPw, newCar, userId);
+    }
+    else if(!newPw){
+        sql='update users set car=? where user_id=?';
+        dataParams.push(newCar, userId);
+    }
+    else if(!newCar){
+        sql='update users set user_pw=? where user_id=?';
+        dataParams.push(newPw, userId);
+    }
+
+    db.query(sql, dataParams, (err, data)=>{
+        if(err){
+            res.status(500).send(err);
+        }
+        else{
+            res.status(200).send({message:'정보가 업데이트되었습니다.'});
+        }
+    });
+});
+
+export default router;
---
+++ project02-server/server.js
@@ -0,0 +1,29 @@
+// node.js 환경에서는 import.meta.env 사용 X
+// 추가로 .env 파일을 인식하도록 선언
+import dotenv from 'dotenv';
+dotenv.config();
+
+// express 모듈 호출
+import express from 'express';
+const app=express();
+
+// HTTPOnly cookie 사용
+import cookieParser from 'cookie-parser';
+app.use(cookieParser());
+
+// POST 방식으로 받은 데이터를 읽기 위한 미들웨어 추가
+app.use(express.json());
+
+// 라우터 파일에서 api 처리
+import authRouter from './routes/authRouter.js';
+app.use('/api/auth', authRouter);
+import userRouter from './routes/userRouter.js';
+app.use('/api/user', userRouter);
+import boardRouter from './routes/boardRouter.js'
+app.use('/api/board', boardRouter);
+
+const PORT=process.env.EXPRESS_PORT || 8080;
+
+app.listen(PORT, ()=>{
+    console.log(`Server run : PORT ${PORT}`);
+});
